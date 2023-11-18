2bfab53a42b0f62af88660ed05e52f6697f1cef7
VatRabbit <137120996+VatRabbit@users.noreply.github.com>
2023-11-18 01:16:47 -0600

    ok

--- GameStateAttempt/main.py
+++ GameStateAttempt/main.py
@@ -2,9 +2,9 @@ import pygame, time
 from sys import exit
 
 DISPLAY_SCALE = 2
-SCREEN_WIDTH, SCREEN_HEIGHT = 400, 200  
-SCALED_WIDTH = SCREEN_WIDTH // DISPLAY_SCALE
-SCALED_HEIGHT = SCREEN_HEIGHT // DISPLAY_SCALE
+SCREEN_WIDTH, SCREEN_HEIGHT = 600, 300  
+SCALED_WIDTH = SCREEN_WIDTH / DISPLAY_SCALE
+SCALED_HEIGHT = SCREEN_HEIGHT / DISPLAY_SCALE
 SPEED = 125
 FPS = 60
 
@@ -38,11 +38,11 @@ class Game:
             self.dt = time.time() - last_update
             last_update = time.time()
             
-            self.states[self.game_state_manager.getState()].run(self.events) 
+            self.states[self.game_state_manager.get_state()].run(self.events) 
             
             scaled_display = pygame.transform.scale(self.display, (self.display.get_width() * DISPLAY_SCALE, self.display.get_height() * DISPLAY_SCALE))
             self.display.blit(scaled_display, (0,0))
-            pygame.display.update()            
+            pygame.display.flip()            
             
             self.clock.tick(FPS)
     
@@ -50,33 +50,39 @@ class Game:
         def __init__(self, current_state):
             self.current_state = current_state
     
-        def setState(self, state):
+        def set_state(self, state):
             self.current_state = state
         
-        def getState(self):
+        def get_state(self):
             return self.current_state
     
-    class Menu:
-        def __init__(self, display, gameStateManager):
-            self.display = display
-            self.gameStateManager = gameStateManager
-        
-        def run(self, events):        
-            self.display.fill((140, 140, 110))
-            for event in events:             
-                 if event.type == pygame.KEYDOWN:
-                     self.gameStateManager.setState('level')
-        
     class Level:
         def __init__(self, display, game_state_manager, player):
             self.display = display
             self.game_state_manager = game_state_manager
             self.player = player
+            self.platform = pygame.Rect(SCALED_WIDTH // 2, SCALED_HEIGHT // 1.75, 40 / DISPLAY_SCALE, 40 / DISPLAY_SCALE) 
         
-        def run(self, events):
+        def run(self, events):            
             self.display.fill((110, 140, 140))
+            pygame.draw.rect(self.display, (100,100,250), self.player.collision_rect)
+            if self.platform.colliderect(self.player.rect):
+                pygame.draw.rect(self.display, (100, 200, 200), self.platform)
+            else:
+                pygame.draw.rect(self.display, (200, 100, 200), self.platform)
             self.player.update(events)
+
+    class Menu:
+        def __init__(self, display, game_state_manager):
+            self.display = display
+            self.game_state_manager = game_state_manager
         
+        def run(self, events):        
+            self.display.fill((140, 140, 110))
+            for event in events:             
+                 if event.type == pygame.KEYDOWN:
+                     self.game_state_manager.set_state('level')
+                
 class Player(pygame.sprite.Sprite):
     def __init__(self, display):
         super().__init__()
@@ -90,61 +96,12 @@ class Player(pygame.sprite.Sprite):
         self.animation_states = {'idle': self.animation_idle, 'run': self.animation_run, 'jump': self.animation_jump}
         
         self.sprites = self.sprite_list_idle
-        self.sprite_rect = self.sprites[0].get_rect(bottomleft = (20,100))
+        self.rect = self.sprites[0].get_rect(bottomleft = (20,100))
+        self.collision_rect = pygame.Rect(0,0,10,20)
         
         self.reverse = False
         self.gravity = 0     
-        self.x, self.y = 50.0 / DISPLAY_SCALE, 200.0 / DISPLAY_SCALE
-
-    def update(self, events):                  
-        self.handle_input(events, game.dt)   
-        self.apply_gravity(game.dt) 
-        self.update_player_rect() 
-        self.animation_states[self.animation_state_manager.get_state()].run(self.sprite_rect, self.reverse)
-
-    def update_player_rect(self):
-        if self.x < 0:
-            self.x = 0
-        elif self.x + self.sprite_rect.width > SCALED_WIDTH:
-            self.x = SCALED_WIDTH - self.sprite_rect.width
-
-        self.sprite_rect.bottomleft = (self.x,self.y)
-           
-    def load_sprite_sheet(self):
-        # 198 x 192p spritesheet with 6 collumbs and 6 rows 
-        sprites = []
-        spritesNumberWidth = 6
-        spritesNumberHeight = 6
-        spriteSheet = pygame.image.load('player/player.png').convert_alpha()
-        spriteSheetWidth = spriteSheet.get_width()
-        spriteSheetHeight = spriteSheet.get_height()
-        spriteWidth = spriteSheetWidth // spritesNumberWidth
-        spriteHeight = spriteSheetHeight // spritesNumberHeight
-        
-        for y in range(spritesNumberWidth):
-            for x in range(spritesNumberHeight):                        
-                spriteX = x * spriteWidth
-                spriteY = y * spriteHeight                
-                
-                spriteRect = pygame.Rect(spriteX, spriteY, spriteWidth, spriteHeight)
-                
-                isBlank = True
-                for i in range(spriteRect.width):
-                    for j in range(spriteRect.height):
-                        pixelColor = spriteSheet.get_at((spriteRect.x + i, spriteRect.y + j))
-                        if pixelColor[3] != 0: # check the alpha channel
-                            isBlank = False
-                            break
-
-                if not isBlank:
-                    sprites.append(spriteSheet.subsurface(spriteRect))
-        
-        self.sprite_list_idle   = [sprites[0],  sprites[1],  sprites[2],  sprites[3]]
-        self.sprite_list_run    = [sprites[4],  sprites[5],  sprites[6],  sprites[7], sprites[8],  sprites[9]]
-        self.sprite_list_climb  = [sprites[10], sprites[11], sprites[12], sprites[13]]
-        self.sprite_list_crouch = [sprites[14], sprites[15], sprites[16]]
-        self.sprite_list_death  = [sprites[17], sprites[18]]
-        self.sprite_list_jump   = [sprites[19], sprites[20]]  
+        self.x, self.y = 50.0 / DISPLAY_SCALE, 300.0 / DISPLAY_SCALE
         
     def apply_gravity(self, dt):
         self.y += self.gravity * dt
@@ -155,7 +112,11 @@ class Player(pygame.sprite.Sprite):
             
         if self.y > SCREEN_HEIGHT / DISPLAY_SCALE:
             self.y = SCREEN_HEIGHT / DISPLAY_SCALE
-                
+            
+    def handle_collisions(self, colliders):
+        for collision in colliders:
+            pass
+
     def handle_input(self, events, dt):
         keys = pygame.key.get_pressed() 
         
@@ -163,7 +124,7 @@ class Player(pygame.sprite.Sprite):
              for event in events:
                  if event.type == pygame.KEYDOWN:
                      if event.key == pygame.K_SPACE:                         
-                         self.gravity = -300                
+                         self.gravity = -400                
 
         if keys[pygame.K_LEFT] == True and keys[pygame.K_RIGHT] == True:
             self.animation_state_manager.set_state('idle')
@@ -199,6 +160,62 @@ class Player(pygame.sprite.Sprite):
                         
         if self.y < SCREEN_HEIGHT / DISPLAY_SCALE:
             self.animation_state_manager.set_state('jump')
+             
+    def load_sprite_sheet(self):
+        # 198 x 192p spritesheet with 6 collumbs and 6 rows 
+        sprites = []
+        spritesNumberWidth = 6
+        spritesNumberHeight = 6
+        spriteSheet = pygame.image.load('player/player.png').convert_alpha()
+        spriteSheetWidth = spriteSheet.get_width()
+        spriteSheetHeight = spriteSheet.get_height()
+        spriteWidth = spriteSheetWidth // spritesNumberWidth
+        spriteHeight = spriteSheetHeight // spritesNumberHeight
+        
+        for y in range(spritesNumberWidth):
+            for x in range(spritesNumberHeight):                        
+                spriteX = x * spriteWidth
+                spriteY = y * spriteHeight                
+                
+                spriteRect = pygame.Rect(spriteX, spriteY, spriteWidth, spriteHeight)
+                
+                isBlank = True
+                for i in range(spriteRect.width):
+                    for j in range(spriteRect.height):
+                        pixelColor = spriteSheet.get_at((spriteRect.x + i, spriteRect.y + j))
+                        if pixelColor[3] != 0: # check the alpha channel
+                            isBlank = False
+                            break
+
+                if not isBlank:
+                    sprites.append(spriteSheet.subsurface(spriteRect))
+        
+        self.sprite_list_idle   = [sprites[0],  sprites[1],  sprites[2],  sprites[3]]
+        self.sprite_list_run    = [sprites[4],  sprites[5],  sprites[6],  sprites[7], sprites[8],  sprites[9]]
+        self.sprite_list_climb  = [sprites[10], sprites[11], sprites[12], sprites[13]]
+        self.sprite_list_crouch = [sprites[14], sprites[15], sprites[16]]
+        self.sprite_list_death  = [sprites[17], sprites[18]]
+        self.sprite_list_jump   = [sprites[19], sprites[20]]  
+
+    def update(self, events):
+        self.handle_input(events, game.dt)   
+        self.apply_gravity(game.dt)
+        # self.check_collisions() ?
+        self.update_player_rect() 
+        self.animation_states[self.animation_state_manager.get_state()].run(self.rect, self.reverse)
+
+    def update_player_rect(self):
+        print(self.x)
+        
+        if self.x < 0:
+            self.x = 0
+            print('1')
+        elif (self.x + self.collision_rect.width * DISPLAY_SCALE) > SCALED_WIDTH:
+            self.x = SCALED_WIDTH - self.collision_rect.width * DISPLAY_SCALE
+            print('2')
+            
+        self.collision_rect.bottomleft = (self.x,self.y)  
+        self.rect.midbottom = self.collision_rect.midbottom
         
     class Animation_State_Manager:
         def __init__(self, current_state):
@@ -232,7 +249,20 @@ class Player(pygame.sprite.Sprite):
                 self.display.blit(reverse_sprite, rect)
             else:
                 self.display.blit(self.sprites[self.frame], rect)
+                
+    class Animation_Jump:
+        def __init__(self, display, sprites):
+            self.display = display
+            self.sprites = sprites
+            self.frame = 0
             
+        def run(self, rect, reverse):                    
+            if reverse == True:
+                reverse_sprite = pygame.transform.flip(self.sprites[self.frame], True, False)
+                self.display.blit(reverse_sprite, rect)
+            else:
+                self.display.blit(self.sprites[self.frame], rect)   
+                
     class Animation_Run:
         def __init__(self, display, sprites):
             self.display = display
@@ -250,19 +280,6 @@ class Player(pygame.sprite.Sprite):
                 if (self.frame >= len(self.sprites)):
                     self.frame = 0
                     
-            if reverse == True:
-                reverse_sprite = pygame.transform.flip(self.sprites[self.frame], True, False)
-                self.display.blit(reverse_sprite, rect)
-            else:
-                self.display.blit(self.sprites[self.frame], rect)   
-                
-    class Animation_Jump:
-        def __init__(self, display, sprites):
-            self.display = display
-            self.sprites = sprites
-            self.frame = 0
-            
-        def run(self, rect, reverse):                    
             if reverse == True:
                 reverse_sprite = pygame.transform.flip(self.sprites[self.frame], True, False)
                 self.display.blit(reverse_sprite, rect)

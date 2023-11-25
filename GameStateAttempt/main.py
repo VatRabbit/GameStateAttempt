95b614d2a443027a765ccf5624a67e5e906c39fc
VatRabbit <137120996+VatRabbit@users.noreply.github.com>
2023-11-24 20:13:24 -0600

    stable

--- GameStateAttempt/main.py
+++ GameStateAttempt/main.py
@@ -4,6 +4,8 @@ TO ADD:
 - scrolling level
 - camera
 - enemy
+- jump buffer
+- variable jump height
 '''
 
 import pygame, time, player
@@ -21,16 +23,16 @@ class Game:
         pygame.init()
         self.display = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         self.clock = pygame.time.Clock()
-        pygame.display.set_caption('Crappy Game')
+        pygame.display.set_caption('Crappy Platformer')
         self.dt = 0.0
                
-        self.camera = self.Camera()
+        self.camera = self.Camera(self.display)
         self.player = player.Player(self.display)
         
         self.game_state_manager = self.Game_State_Manager('menu')  
-        self.menu = self.Menu(self.display, self.game_state_manager, self.camera)         
-        self.level = self.Level(self.display, self.game_state_manager, self.player, self.camera, self.dt)         
-        self.states = {'level': self.level, 'menu': self.menu}           
+        self.menu = self.Menu(self.display, self.game_state_manager, self.camera)
+        self.level = self.Level(self.display, self.game_state_manager, self.player, self.camera, self.dt)
+        self.states = {'level': self.level, 'menu': self.menu}
         self.last_update = 0.0
         
     def mainLoop(self):
@@ -50,6 +52,7 @@ class Game:
     def timer(self):
         self.dt = time.time() - self.last_update
         self.last_update = time.time()
+        # print(self.dt)
             
     def render(self):
         scaled_display = pygame.transform.scale(self.display, (self.display.get_width() * DISPLAY_SCALE, self.display.get_height() * DISPLAY_SCALE))
@@ -57,9 +60,20 @@ class Game:
         pygame.display.flip()        
 
     class Camera:
-        def __init__(self):
+        def __init__(self, display):
+            display = display
             self.x = 0.0
             self.y = 0.0
+                        
+        def get_x(self):
+            return self.x
+        def add_x(self, x):
+            self.x += x
+            
+        def get_y(self):
+            return self.y
+        def add_y(self, y):
+            self.y += y            
     
     class Game_State_Manager:
         def __init__(self, current_state):
@@ -74,8 +88,8 @@ class Game:
     class Level:
         def __init__(self, display, game_state_manager, player, camera, dt):
             self.display = display
-            self.game_state_manager = game_state_manager           
-            self.player = player     
+            self.game_state_manager = game_state_manager
+            self.player = player
             self.player.x = 0
             self.player.y = 0
             self.collision_check_list = []
@@ -95,16 +109,14 @@ class Game:
                 [1,0,0,0,0,0,0,0,0,0,1,1,1,1],
                 [1,1,1,1,1,1,1,1,1,1,1,1,1,1],
             ]
-
             self.tile_rect_list = self.create_tile_rects()          
                     
         def run(self, events, dt):
-            self.handle_input()
+            self.handle_input(dt)
+            self.update_tile_rects()
             self.collision_check_list = self.check_collisions()               
             self.player.update(events, dt, self.collision_check_list)
-            self.update_tile_rects()
-            self.render() 
-            self.player.render()
+            self.render()             
             
         def create_tile_rects(self):
             rect_list = []    
@@ -120,32 +132,30 @@ class Game:
                         self.player.x = y * TILE_SIZE
                         self.player.y = x * TILE_SIZE + TILE_SIZE 
                         
-            return rect_list                        
+            return rect_list
 
         def update_tile_rects(self):
-            rect_list = []    
+            rect_list = []
+            cam_x = self.camera.get_x()
+            cam_y = self.camera.get_y()
         
             for x in range(len(self.level_tiles)):
                 for y in range(len(self.level_tiles[0])):
                     if self.level_tiles[x][y] == 1:
-                        # draw tile sprites here later instead of rect    
-                        rect = pygame.Rect(y * TILE_SIZE + self.camera.y, x * TILE_SIZE + self.camera.x, TILE_SIZE, TILE_SIZE)    
+                        # draw tile sprites here later instead of rect
+                        rect = pygame.Rect(y * TILE_SIZE + cam_x, x * TILE_SIZE + cam_y, TILE_SIZE, TILE_SIZE)
                         rect_list.append(rect)                 
                         
-            return rect_list
-
-        def blit_tiles(self):
-            for rect in self.tile_rect_list:
-                pygame.draw.rect(self.display, (100,100,250), rect, 2) 
+            self.tile_rect_list = rect_list
                                 
-        def check_collisions(self):                   
+        def check_collisions(self):
             check_list = []
             
             # take in the 3x3 grid surrounding the player to check for collisions
             for i in range(-1, 2):
                 for j in range(-2, 3):
-                    grid_y = int(self.player.x / TILE_SIZE + i)
-                    grid_x = int(self.player.y / TILE_SIZE + j - 1)
+                    grid_y = int((self.player.x - self.camera.x) / TILE_SIZE + i)
+                    grid_x = int((self.player.y - self.camera.y) / TILE_SIZE + j - 1)
                     
                     if 0 <= grid_x < len(self.level_tiles) and 0 <= grid_y < len(self.level_tiles[0]):
                          if self.level_tiles[grid_x][grid_y] == 1:                                
@@ -153,28 +163,37 @@ class Game:
                               check_list.append(rect)
 
             return check_list
+        
+        def show_collision_check_list(self):
+            for rect in self.collision_check_list:
+                pygame.draw.rect(self.display, (100,100,250), rect)
                 
         def reset(self):
             pass    
         
-        def handle_input(self):            
+        def handle_input(self, dt):
             keys = pygame.key.get_pressed()
             
             if keys[pygame.K_LCTRL] == True:
-                print('left ctrl down')
-                self.camera.y += 100 * self.dt
+                # print('left ctrl down. Adding ', 100 * dt)
+                self.camera.add_x(100 * dt)
+                self.player.x += 100 * dt
                 
             if keys[pygame.K_LALT] == True:
-                print('left alt down')
-                self.camera.y -= 100 * self.dt
+                # print('left alt down. Adding ', -100 * dt)
+                self.camera.add_x(-100 * dt)
+                self.player.x += -100 * dt
                 
-            print(self.camera.y)
+            # print(self.camera.get_x())
+            # print(self.dt)
         
         def render(self):
             self.display.fill((110, 140, 140))    
-            self.blit_tiles()
-            for tile in self.collision_check_list:
-                pygame.draw.rect(self.display, (100,100,250), tile)             
+            for rect in self.tile_rect_list:
+                pygame.draw.rect(self.display, (100,100,250), rect, 2)    
+
+            self.show_collision_check_list()
+            self.player.render()
             
     class Menu:
         def __init__(self, display, game_state_manager, camera):

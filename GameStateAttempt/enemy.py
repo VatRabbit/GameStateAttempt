d61949e99405e940e8edce275b3f2f73cab88762
VatRabbit <137120996+VatRabbit@users.noreply.github.com>
2024-05-05 22:38:39 -0500

    Fixed issue with bunnies reversing too often by adding a cd to it

--- GameStateAttempt/enemy.py
+++ GameStateAttempt/enemy.py
@@ -1,11 +1,12 @@
 '''
 To Do:
-- swap out self.rect for self.collision_rect where needed
+
+ISSUES:
 '''
 
 import pygame
 
-SPEED = 40
+SPEED = 50
 ANIMATION_SPEED = 7
 
 class enemy(pygame.sprite.Sprite):
@@ -16,15 +17,14 @@ class enemy(pygame.sprite.Sprite):
         self.position        = [x, y]
         self.rect            = pygame.Rect(0,0, 24,24)
         self.collision_rect  = pygame.Rect(0,0, 14,16)
-        # self.collision_list  = []
-        # self.collision_range = []
         
-        # reverse = left
         # could change this out for a + or - velocity check instead
         # and then multiply velocity by -1
-        self.reverse     = True
-        self.ground_rect = pygame.Rect(0,0, TILE_SIZE,TILE_SIZE) 
-        self.wall_rect   = pygame.Rect(0,0, TILE_SIZE,TILE_SIZE)
+        self.reverse       = True
+        self.reverse_timer = 0.0
+        self.reverse_cd    = 0.1
+        self.ground_rect   = pygame.Rect(0,0, TILE_SIZE,TILE_SIZE) 
+        self.wall_rect     = pygame.Rect(0,0, TILE_SIZE,TILE_SIZE)
         
         self.animation_state_manager = self.Animation_State_Manager('jump')
         self.animation_jump          = self.Animation_Jump(display, sprites)
@@ -33,32 +33,39 @@ class enemy(pygame.sprite.Sprite):
     # carry out enemy logic, set velocity and direction, and set animation states
     def AI(self, dt, tilemap, TILE_SIZE):
         self.collision_list = self.check_collisions(TILE_SIZE)
-        grounded = self.check_ground(tilemap, TILE_SIZE)
+        grounded = self.check_ground(tilemap, TILE_SIZE)        
         
-        if self.reverse:
+        if self.reverse and self.reverse_timer > self.reverse_cd:
             # all I really need is the y and x coordinates of wall_rect, really. Rect could be a waste
             if tilemap[int(self.wall_rect.y / TILE_SIZE)][int(self.wall_rect.x / TILE_SIZE)] == 1:
                 if self.wall_rect.right > self.collision_rect.left:
                      self.collision_rect.left = self.wall_rect.right
                      self.reverse = False 
-            if grounded == False:
+                     self.reverse_timer = 0
+            if grounded == False:                
                  self.reverse = False
-        else:
+                 self.reverse_timer = 0 
+                 
+        elif not self.reverse and self.reverse_timer > self.reverse_cd:
             if tilemap[int(self.wall_rect.y / TILE_SIZE)][int(self.wall_rect.x / TILE_SIZE)] == 1:
                 if self.wall_rect.left < self.collision_rect.right:
                     self.collision_rect.right = self.wall_rect.left
                     self.reverse = True
+                    self.reverse_timer = 0
             if grounded == False:
                 self.reverse = True
+                self.reverse_timer = 0
                     
         if self.reverse:
             self.velocity[0] = dt * SPEED * -1
         else:
             self.velocity[0] = dt * SPEED
-            
+        
+        self.reverse_timer += dt
+        # print(self.reverse_timer)
         self.position[0] += self.velocity[0]
         self.rect.x       = int(self.position[0])
-        self.rect.y       = int(self.position[1]) - 8
+        self.rect.y       = int(self.position[1]) - (self.rect.height - TILE_SIZE)
         self.collision_rect.midbottom = self.rect.midbottom
         
     # apply velocity to enemy
@@ -135,8 +142,7 @@ class enemy(pygame.sprite.Sprite):
             self.true_frame += dt * ANIMATION_SPEED
             if self.true_frame >= 1:
                 self.frame += 1
-                self.true_frame -= 1
-                print(len(self.sprites))
+                self.true_frame -= 1                
                 if self.frame > len(self.sprites) - 1:
                     self.frame = 0
                     
